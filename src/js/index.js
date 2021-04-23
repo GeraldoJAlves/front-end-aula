8750cf5574be0051b661f5ed201facd516039a19
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-04-23 20:51:55 -0300

    leitura da imagem no navegador

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,41 @@ const listFiles = () => {
   const files = inputUpload[0].files;
 
   if (files.length > 0) {
+    console.log(files);
     document.querySelector('body > main').classList.add('hidden-element');
     document.querySelector('body > div').classList.remove('hidden-element');
+
+    for(let file of files) {
+      console.log(file);
+      makeList(file);
+    }
+
+    // for(let x=0; x < files.length; x++) {
+    //   console.log(files[x]);
+    //   makeList(files[x]);
+    // }
   }
+}
+
+const makeList = (file) => {
+  // <li></li>
+  const li = document.createElement('li');
+  li.innerText = file.name;
+  const img = document.createElement('img');
+
+  //object url
+  const url = URL.createObjectURL(file);
+  img.src = url;
+  li.appendChild(img);
+  document.querySelector('.list-files').appendChild(li);
+
+  // leitura como base64
+  // const fileReader = new FileReader();
+  // fileReader.addEventListener('load', () => {
+  //   img.src = fileReader.result;
+  //   li.appendChild(img);
+  //   document.querySelector('.list-files').appendChild(li);
+  // });
+
+  // fileReader.readAsDataURL(file);
 }
