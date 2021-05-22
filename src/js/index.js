58d6570b7d23983f7a973596e7a1a1a587d61d9c
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-05-21 21:45:30 -0300

    Adicionando estado na lista de imagens e criando ações

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+let listFilesUpload = [];
+let indexCurrentImage = 0;
+
 const uploadFile = () => {
   const inputUpload = document.getElementsByName('file-upload');
   inputUpload[0].click();
@@ -8,29 +11,51 @@ const listFiles = () => {
   const files = inputUpload[0].files;
 
   if (files.length > 0) {
-    console.log(files);
+    listFilesUpload = [];
     document.querySelector('body > main').classList.add('hidden-element');
     document.querySelector('body > div').classList.remove('hidden-element');
 
-    let items = '';
     for(let file of files) {
-      items += makeItemFile(file);
+      listFilesUpload.push(makeItemFile(file));
     }
-    
-    document.querySelector('.list-files').innerHTML = items;
+    setImagePreview(0);
   }
 }
 
 const makeItemFile = (file) => {
-  const url = URL.createObjectURL(file);
-  return `
-    <li>
-      <div>
-        <i class="fas fa-chevron-down"></i>
-        <span>${file.name}</span>
-        <i class="fas fa-trash"></i>
-      </div>
-      <img src="${url}" />
-    </li>
-  `;
+  const src = URL.createObjectURL(file);
+  return {
+    src,
+    name: file.name
+  }
+}
+
+const setImagePreview = (indexFile) => {
+  indexCurrentImage = indexFile;
+  const {src, name} = listFilesUpload[indexFile];
+  document.querySelector('.list-files img').setAttribute('src', src);
+  document.querySelector('.list-files span').innerText = name;
+  document.querySelector('.base-menu.footer span').innerText =
+    `${indexCurrentImage + 1} / ${listFilesUpload.length}`;
+
+  const fastBackward = document.querySelector('.base-menu i.fa-fast-backward');
+  const stepBackward = document.querySelector('.base-menu i.fa-step-backward');
+  const stepForward = document.querySelector('.base-menu i.fa-step-forward');
+  const fastForward = document.querySelector('.base-menu i.fa-fast-forward');
+
+  if (indexCurrentImage === 0) {
+    fastBackward.classList.add('disable-action');
+    stepBackward.classList.add('disable-action');
+  } else {
+    fastBackward.classList.remove('disable-action');
+    stepBackward.classList.remove('disable-action');
+  }
+
+  if (indexCurrentImage === (listFilesUpload.length - 1) ) {
+    stepForward.classList.add('disable-action');
+    fastForward.classList.add('disable-action');
+  } else {
+    stepForward.classList.remove('disable-action');
+    fastForward.classList.remove('disable-action');
+  }
 }
