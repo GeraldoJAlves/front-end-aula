a87587073e1fd407c7f0c1a531dbd91fac651911
GeraldoJAlves <junior.alves.2007@gmail.com>
2021-06-21 20:14:34 -0300

    importando fontawesome

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express();
-app.use('/assets', express.static('assets'));
+app.use('/assets/libs/fontawesome-free', express.static('./node_modules/@fortawesome/fontawesome-free'));
 app.use('/', express.static('src'));
 
 app.listen(3000)
